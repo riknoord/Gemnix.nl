835058980984171695f7b5dd069331fdc7ac677f
Rik van Noord <riknoord@hotmail.com>
2017-10-01 20:49:06 +0200

    First Commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+        <style>
+            img {
+                width:250px;
+                height:250px;
+                position: absolute;
+                top: 50%;
+                left: 50%;
+                margin-left: -125px;
+                margin-top: -125px;
+            }
+        </style>
+
+    </head>
+    <body>
+        <img src="/img/logo.svg" />
+    </body>
+</html>
