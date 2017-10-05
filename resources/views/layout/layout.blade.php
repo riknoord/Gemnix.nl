f292945709fb2b5463a6e83417c20ff3f60f4862
Rik van Noord <riknoord@hotmail.com>
2017-10-05 20:02:19 +0200

    Viewport scale fix

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>GEMNIX</title>
 
     <link href="/css/app.css" rel="stylesheet" type="text/css">
