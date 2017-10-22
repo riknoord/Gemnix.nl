31ef951e92e009d987c91e3711e3c288629f4604
Rik van Noord <riknoord@hotmail.com>
2017-10-22 12:42:45 +0200

    Start on GnxCms

---
+++ GnxCms/src/Controllers/Cms/LoginController.php
@@ -0,0 +1,14 @@
+<?php
+namespace Gemnix\GnxCms\Controllers\Cms;
+
+use App\Http\Controllers\Controller;
+
+class LoginController extends controller{
+
+    public function index(){
+
+        echo "hallo";
+
+    }
+
+}
