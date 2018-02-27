fb9d76b342e3e14b16b710b30036b3bd238eeb97
Rik van Noord <riknoord@hotmail.com>
2018-02-27 23:04:36 +0100

    Check for sl header

--- app/Http/Controllers/Manager/ServerController.php
+++ app/Http/Controllers/Manager/ServerController.php
@@ -8,6 +8,8 @@ use App\Http\Controllers\Controller;
 class ServerController extends Controller
 {
     public function index(Request $request){
-        print_r($request->headers);
+        if($request->hasHeader('X-Secondlife-Object-Name')){
+            print_r($request->header('X-Secondlife-Object-Name'));
+        }
     }
 }
