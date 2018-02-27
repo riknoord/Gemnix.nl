84774cc05fa840cae28abd857ee45e1f880f22f3
Rik van Noord <riknoord@hotmail.com>
2018-02-27 23:09:20 +0100

    test3

--- app/Http/Controllers/Manager/ServerController.php
+++ app/Http/Controllers/Manager/ServerController.php
@@ -8,10 +8,11 @@ use App\Http\Controllers\Controller;
 class ServerController extends Controller
 {
     public function index(Request $request){
-        if($request->hasHeader('X-Secondlife-Object-Name')){
-            print_r($request->header('X-Secondlife-Object-Name'));
+        if($request->hasHeader('x-secondlife-owner-name')){
+            print_r($request->header('x-secondlife-owner-name'));
         }
 
         print_r($request->headers);
+        print_r($request->header('accept'));
     }
 }
